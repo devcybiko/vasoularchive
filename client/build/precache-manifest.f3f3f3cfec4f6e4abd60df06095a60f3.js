f7d4f0519988d5000137e3b92c84492b7b671069
aaron <aaronbushman@gmail.com>
2019-10-10 16:04:14 -0400

    adding build

---
+++ client/build/precache-manifest.f3f3f3cfec4f6e4abd60df06095a60f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3467e3685a7d528b9c08413725edee04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde795241a8a57e0a7bf",
+    "url": "/static/css/main.70f1e6ce.chunk.css"
+  },
+  {
+    "revision": "1812b94e3ff8acdc0df6",
+    "url": "/static/js/2.7d94d929.chunk.js"
+  },
+  {
+    "revision": "bde795241a8a57e0a7bf",
+    "url": "/static/js/main.80e11655.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
